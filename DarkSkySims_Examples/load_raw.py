f45dad4c83c5ee41aab148fd74dbd6a14a1a0705
Jayke Nguyen <jsn.phpro1701@gmail.com>
2016-09-09 01:36:26 -0700

    rearranging files

--- DarkSkySims_Examples/load_raw.py
+++
@@ -1,7 +0,0 @@
-from yt.utilities.sdf import load_sdf
-
-filename = "http://darksky.slac.stanford.edu/simulations/ds14_a/ds14_a_1.0000"
-sdfdata = load_sdf(filename)
-
-print 'Total particles: ', sdfdata['x'].size
-
--- DarkSkySims_Examples/test_gadget.py
+++
@@ -1,5 +0,0 @@
-import yt
-ds = yt.load(
-    "http://portal.nersc.gov/project/darksky/ds14_a/ds14_a/ic.0",
-    sdf_header="http://portal.nersc.gov/project/darksky/ds14_a/ds14_a/gadget_data.hdr")
-
